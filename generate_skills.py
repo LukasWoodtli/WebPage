ca7bad1b97922a3d8d2091fbaa9c4c9e5864dbca
Lukas Woodtli <woodtli.lukas@gmail.com>
2015-03-07 17:23:33 +0100

    Improve skript that generates skills list

--- generate_skills.py
+++ generate_skills.py
@@ -1,6 +1,9 @@
 import os
 import csv
 
+THIS_FILES_PATH = os.path.realpath(__file__)
+THIS_FILES_FOLDER_PATH = os.path.split(THIS_FILES_PATH)[0]
+
 OUT_PATH = "/Users/Boot/projects/yoursite/content/pages/"
 
 class Skill:
@@ -19,18 +22,18 @@ class Skill:
 
 def get_skills(file):
     csv_file  = open(file)
-    return csv.reader(csv_file, delimiter=';')
+    return csv.DictReader(csv_file, ["skill", "category", "rating"], delimiter=';')
 
 
 
 def main():
     categories = {}
-    for skill in get_skills("/Users/Boot/Bewerbung/Skills.csv"):
-        skill = Skill(skill)
-        if skill.category in categories.keys():
-            categories[skill.category].append(skill)
+    for skill in get_skills(os.path.join(THIS_FILES_FOLDER_PATH, "Skills.csv")):
+        category = skill["category"].strip()
+        if category in categories.keys():
+            categories[category].append(skill)
         else:
-            categories[skill.category] = [skill]
+            categories[category] = [skill]
 
     with open(os.path.join(OUT_PATH, "skills.md"), 'w') as out_file:
         out_file.write("Title: Skills" + "\n")
@@ -39,7 +42,7 @@ def main():
             out_file.write("what | knowledge\n")
             out_file.write("---|--- :\n")
             for skill in skills:
-                out_file.write(str(skill) + "\n")
+                out_file.write(skill["skill"].strip() + "|" + skill["rating"].strip() + "\n")
             out_file.write("\n")
 
 
