3c71f151a615a3019c388b4bb43bd1ec2e040160
Lukas Woodtli <woodtli.lukas@gmail.com>
2015-03-09 21:54:33 +0100

    Worked on script for generating skills list

--- data/generate_skills.py
+++ data/generate_skills.py
@@ -2,11 +2,11 @@ import os
 import csv
 from collections import namedtuple
 
-Page = namedtuple('Page', 'Title SectionColumn AllColumns')
+Page = namedtuple('Page', 'FileName Title SectionColumn AllColumns')
 
-SkillsPage = Page("Skills", "category", ["skill", "category", "rating"])
-BooksPage = Page("Bookshelf", "category", ["name", "category", "authors", "publisher"])
 
+SkillsPage = Page("skills.csv", "Skills", "category", ["skill", "category", "rating"])
+BooksPage = Page("bookshelf.csv", "Bookshelf", "category", ["name", "category", "authors", "publisher"])
 
 
 THIS_FILES_PATH = os.path.realpath(__file__)
@@ -17,24 +17,31 @@ OUT_PATH = "/Users/Boot/projects/yoursite/content/pages/"
 COLUMN_WIDTH = 60
 
 
-def get_skills(file):
+def get_skills(file, columns):
     csv_file  = open(file)
-    return csv.DictReader(csv_file, ["skill", "category", "rating"], delimiter=';')
+    return csv.DictReader(csv_file, columns, delimiter=';')
 
 def create_md_row(first_col, second_col):
-    return first_col + " "*(COLUMN_WIDTH-len(first_col)) + "|" + second_col + " "*(COLUMN_WIDTH-len(second_col)) + "\n"
+    return "| " + first_col + " | " + second_col  + " |\n"
 
 def main():
+
+    data = SkillsPage # make loop
+
+    fileName = data.FileName
+    title = data.Title
+    sectionColumn = data.SectionColumn
+    allColumns = data.AllColumns
     categories = {}
-    for skill in get_skills(os.path.join(THIS_FILES_FOLDER_PATH, "Skills.csv")):
-        category = skill["category"].strip()
+    for skill in get_skills(os.path.join(THIS_FILES_FOLDER_PATH, fileName), allColumns):
+        category = skill[sectionColumn].strip()
         if category in categories.keys():
             categories[category].append(skill)
         else:
             categories[category] = [skill]
 
     with open(os.path.join(OUT_PATH, "skills.md"), 'w') as out_file:
-        out_file.write("Title: Skills" + "\n")
+        out_file.write("Title: " + title + "\n")
         for category, skills  in categories.iteritems():
             out_file.write("#" + category + "\n")
             out_file.write(create_md_row("what", "knowledge"))
