1fc9cc94a43864d03cd3711915abface55ed2eca
Lukas Woodtli <woodtli.lukas@gmail.com>
2015-04-30 09:16:17 +0200

    Aproach to fix blog problems (tags, categories..)

--- pelican/pelicanconf.py
+++ pelican/pelicanconf.py
@@ -34,7 +34,13 @@ AUTHOR_FEED_RSS = None
 #SOCIAL = (('You can add links in your config file', '#'),
 #          ('Another social link', '#'),)
 
-DEFAULT_PAGINATION = 10
+DEFAULT_PAGINATION = 2
+TAG_URL = 'tags/{slug}.html'
+TAG_SAVE_AS = 'tags/{slug}.html'
+PAGINATION_PATTERNS = (
+    (1, '{name}.html', '{name}.html'),
+    (2, '{name}/page/{number}.html', '{name}/page/{number}.html'),
+)
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
@@ -48,7 +54,7 @@ THEME = "pelican-chameleon"
 BS3_THEME = "http://bootswatch.com/yeti/bootstrap.min.css"
 
 # Keep the generated blog index but save it under different name
-#INDEX_SAVE_AS = 'blog_index.html'
+INDEX_SAVE_AS = 'blog_index.html'
 
 MENUITEMS = [
     ('Home', '/index.html'),
@@ -58,12 +64,11 @@ MENUITEMS = [
         ('Courses', '/pages/courses.html'),
         ('Books', '/pages/books.html'),
         ('Projects', '/pages/projects.html')]),
-    ('Contact', '/pages/contact.html')
-# ('Archives', [
-    #     ('Tags', '/tags.html'),
-    #     ('Categories', '/categories.html'),
-    #     ('Chronological', '/archives.html'),
-    #     ]),
+    ('Contact', '/pages/contact.html'),
+    ('Blog', [
+         ('Tags', '/tags.html'),
+         ('Categories', '/categories.html'),
+         ('Chronological', '/archives.html')])
     #('Social', [
     #    ('Email', 'mailto:woodtli.lukas@gmail.com'),
     #    ('Github', 'https://github.com/LukasWoodtli'),
