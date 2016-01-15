e93efd69ccc47a0585a799e9c60e39eda05a5a7a
Lukas Woodtli <woodtli.lukas@gmail.com>
2016-01-15 22:47:04 +0100

    Add markdown plug in for HTML attributes

--- pelican/pelicanconf.py
+++ pelican/pelicanconf.py
@@ -48,7 +48,7 @@ home = expanduser("~")
 PLUGIN_PATHS = ["plugins", home, join(home, "pelican-plugins")]
 PLUGINS = ["render_math", "pelican-bootstrapify"] # "better_tables"
 
-MD_EXTENSIONS = ['codehilite(css_class=highlight)','extra', 'sane_lists', 'toc', 'footnotes']
+MD_EXTENSIONS = ['codehilite(css_class=highlight)','extra', 'sane_lists', 'toc', 'footnotes', 'attr_list']
 
 DOCUTILS_SETTINGS = {'table_style' : 'borderless'} # 'math_output': 'mathjax'
 
