54be67a0dc1d7b8910389fecb49ca0be55481c49
Lukas Woodtli <woodtli.lukas@gmail.com>
2018-05-28 22:58:53 +0200

    Fix social links

--- pelican/pelicanconf.py
+++ pelican/pelicanconf.py
@@ -71,8 +71,9 @@ THEME = "pelican-elegant"
 
 SOCIAL = [('LinkedIn', 'https://www.linkedin.com/in/lukaswoodtli'),
           ('github', 'https://github.com/LukasWoodtli'),
-          ('stack overflow', 'https://careers.stackoverflow.com/lukaswoodtli'),
-          ('XING', 'https://www.xing.com/profile/Lukas_Woodtli')]
+          ('stack-overflow', 'https://careers.stackoverflow.com/lukaswoodtli'),
+          ('XING', 'https://www.xing.com/profile/Lukas_Woodtli'),
+          ('email','/pages/contact.html')]
 
 # Keep the generated blog index but save it under different name
 INDEX_SAVE_AS = 'blog_index.html'
