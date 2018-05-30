464c8eb240a8f5ff2a3409de85d6e02cdcf442b1
Lukas Woodtli <woodtli.lukas@gmail.com>
2018-05-30 14:02:09 +0200

    Improve script by storing pelican theme name in a variable

--- generate_page.py
+++ generate_page.py
@@ -19,7 +19,9 @@ try:
 except:
     pass
 
-REPOSITORIES = [("https://github.com/LukasWoodtli/pelican-elegant",           "pelican-elegant"),  # Pelican theme
+PELICAN_THEME = "pelican-elegant" # needs to be set in pelicanconf.py
+
+REPOSITORIES = [("https://github.com/LukasWoodtli/" + PELICAN_THEME,     PELICAN_THEME),  # Pelican theme
                 ("https://github.com/ingwinlu/pelican-bootstrapify",  "pelican-bootstrapify"), # Pelican bootstrapify plug-in
                 ("https://github.com/getpelican/pelican-plugins", "pelican-plugins"),
                 (GITHUB_USERPAGE_REPO, "github-userpage")] # github repo for publishing
@@ -46,10 +48,10 @@ def remove_working_copies_of_repositories():
         remove_local_repository(local_path)
 
 def build_web_page():
-    pelican_chameleon_path = os.path.join(HOME, "pelican-elegant")
-    print "Installing pelican-elegant theme from path: ", pelican_chameleon_path
-    subprocess.call(["pelican-themes", "--remove", "pelican-elegant", "--verbose"])
-    subprocess.call(["pelican-themes", "-i", pelican_chameleon_path, "--verbose"])
+    pelican_theme_path = os.path.join(HOME, PELICAN_THEME)
+    print "Installing pelican-elegant theme from path: ", pelican_theme_path
+    subprocess.call(["pelican-themes", "--remove", PELICAN_THEME, "--verbose"])
+    subprocess.call(["pelican-themes", "-i", pelican_theme_path, "--verbose"])
 
     # make web page
     working_dir = os.path.join(REPO_DIRECTORY, "pelican")
