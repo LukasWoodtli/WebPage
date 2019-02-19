ef78d37b42dea58b201dcb248fbd67d8cdfa80e3
Lukas Woodtli <woodtli.lukas@gmail.com>
2019-02-19 15:51:42 +0100

    Revert "Use original pelican-elegant theme"
    This reverts commit 6bb154ddd171d71e88532e9cd883d8d4698e0470.

--- generate_page.py
+++ generate_page.py
@@ -24,7 +24,7 @@ except:
 
 PELICAN_THEME = "pelican-elegant" # needs to be set in pelicanconf.py
 
-REPOSITORIES = [("https://github.com/Pelican-Elegant/elegant",     PELICAN_THEME),  # Pelican theme
+REPOSITORIES = [("https://github.com/LukasWoodtli/" + PELICAN_THEME,     PELICAN_THEME),  # Pelican theme
                 ("https://github.com/ingwinlu/pelican-bootstrapify",  "pelican-bootstrapify"), # Pelican bootstrapify plug-in
                 ("https://github.com/getpelican/pelican-plugins", "pelican-plugins"),
                 (GITHUB_USERPAGE_REPO, "github-userpage")] # github repo for publishing
