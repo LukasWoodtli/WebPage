27af8f7ce48a37066eaf429fd6dca9e99abbb266
Lukas Woodtli <woodtli.lukas@gmail.com>
2019-05-11 15:12:16 +0200

    Update mathjax Version

--- pelicanconf.py
+++ pelicanconf.py
@@ -85,7 +85,7 @@ MARKDOWN = {
             'output_format': 'html5',
 }
 
-DOCUTILS_SETTINGS = {'table_style' : 'borderless', 'math_output': 'mathjax https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js'}
+DOCUTILS_SETTINGS = {'table_style' : 'borderless', 'math_output': 'mathjax https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js'}
 
 THEME = "pelican-elegant"
 
@@ -107,5 +107,4 @@ MENUITEMS = [
     ('Blog', 'pages/blog.html'),
     ('Contact', 'pages/contact.html')
 ]
-
 
