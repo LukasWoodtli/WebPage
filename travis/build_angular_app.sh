a44d07eda5d03d5444819823cb32120a0ea18c28
Lukas Woodtli <woodtli.lukas@gmail.com>
2021-07-21 16:37:47 +0200

    Run Cypress on travis CI

--- travis/build_angular_app.sh
+++ travis/build_angular_app.sh
@@ -6,23 +6,19 @@ set -u
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # Run this script in container:
-# docker run -it --rm -v `pwd`:/workdir -w /workdir node:15.14.0-alpine3.10 sh
+# docker run -it --rm -v `pwd`:/workdir -w /workdir cypress/browsers:node14.17.0-chrome88-ff89 sh
 
 
 echo Working dir
 pwd
 
-apk add --no-cache chromium xvfb xauth libxtst libnotify-dev nss alsa-lib
-
-export CHROME_BIN=/usr/bin/chromium-browser
-
 echo "Update npm"
 npm install -g npm@7.9.0
 
 echo "Building Web Page"
-export CYPRESS_CACHE_FOLDER="${SCRIPT_DIR}/cypress_cache"
 
 npm install
+npx cypress install --force
 
 
 npm run lint
