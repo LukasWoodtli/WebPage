3566163556ec4aa39a00321e58b78feb7c12fe3a
Lukas Woodtli <woodtli.lukas@gmail.com>
2021-08-05 11:02:06 +0200

    Improve design

--- src/app/pages/table-of-contents/table-of-content.ts
+++ src/app/pages/table-of-contents/table-of-content.ts
@@ -1,7 +1,5 @@
 export class TableOfContentEntry {
 
-    isNewSubHeader = false;
-
     constructor(readonly level: number,
                 readonly text: string,
                 readonly anchor: string) {
