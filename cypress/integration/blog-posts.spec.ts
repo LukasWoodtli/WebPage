feaa6d9abe3984037f55a14cb95987c39a107eea
Lukas Woodtli <woodtli.lukas@gmail.com>
2022-04-05 22:27:54 +0200

    Add test case for dates of blog posts

--- cypress/integration/blog-posts.spec.ts
+++ cypress/integration/blog-posts.spec.ts
@@ -13,6 +13,17 @@ describe('BlogPosts', () => {
     cy.get('[data-test="metadata-sidebar"]').contains("Created");
     cy.get('[data-test="created-date"]').contains("5. September 2019");
     cy.get('[data-test="metadata-sidebar"]').contains("Modified");
+    
+    cy.get('[data-test="modified-date"]', { timeout: 15000 }).invoke('text').then(modifiedDateText => {
+      const dayjs = require('dayjs');
+      var customParseFormat = require('dayjs/plugin/customParseFormat')
+      dayjs.extend(customParseFormat)
+    
+      const modifiedDate = dayjs(modifiedDateText, "D. MMMM YYYY", 'en', true).toDate();
+      const createdDate = dayjs("5. September 2019", "D. MMMM YYYY", 'en', true).toDate();
+
+      expect(modifiedDate).to.be.gte(createdDate);
+  });
   });
 
 });
