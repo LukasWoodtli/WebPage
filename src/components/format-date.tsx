d7332f318cc17938a76d8975f3fd5448faf906e9
Lukas Woodtli <woodtli.lukas@gmail.com>
2022-04-05 22:35:20 +0200

    Replace momentjs with dayjs

--- src/components/format-date.tsx
+++ src/components/format-date.tsx
@@ -1,9 +1,13 @@
 import { Typography } from "@mui/material";
-import moment from "moment";
 import * as React from "react";
 
+const dayjs = require('dayjs');
+var customParseFormat = require('dayjs/plugin/customParseFormat')
+dayjs.extend(customParseFormat)
+
+
 function FormatDate(props: any) {
-  return <Typography>{moment(props.dateTimeStamp * 1000).format("D. MMMM YYYY")}</Typography>;
+  return <Typography>{dayjs(props.dateTimeStamp * 1000).format("D. MMMM YYYY")}</Typography>;
 }
 
 export default FormatDate;
