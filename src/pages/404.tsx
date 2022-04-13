3afbdf4fd1e7c9330d619e7ad8c630b260896475
Lukas Woodtli <woodtli.lukas@gmail.com>
2022-04-13 21:14:29 +0200

    Change 404 page to typescript

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -9,7 +9,7 @@ const NotFoundPage = () => {
     <Layout>
       <GatsbySeo title="404: Not Found" />
       <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <p>You just hit a route that doesn&#39;t exist...</p>
     </Layout>
   );
 };
