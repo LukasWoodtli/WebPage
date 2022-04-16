62316abc2914bcf9fa0b6b3014c019c71e4afd49
Lukas Woodtli <woodtli.lukas@gmail.com>
2022-04-16 22:45:40 +0200

    Fix fonts and icons

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -9,3 +9,11 @@ import "./src/style.css"
 // Highlighting for code blocks
 import "prismjs/themes/prism-solarizedlight.css"
 
+import "@fontsource/roboto"
+import "@fontsource/roboto/300.css"
+import "@fontsource/roboto/400.css"
+import "@fontsource/roboto/500.css"
+import "@fontsource/roboto/700.css"
+
+import "@fontsource/material-icons"
+
