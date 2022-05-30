5f8b2781275cd5b50afe8b493b984f24129611e4
Lukas Woodtli <woodtli.lukas@gmail.com>
2022-05-30 20:58:55 +0200

    Revert "Update Github gatsby page automated."
    This reverts commit c8e4485bc671b29dd7fce01d36622487917331ef.

--- .circleci/user-page-checkout/component---src-templates-static-site-js-77fd950bbc51256da717.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkweb_page=self.webpackChunkweb_page||[]).push([[384],{5466:function(e,t,r){r.r(t);var a=r(7294),n=r(9548),l=r(1152);t.default=function(e){var t=e.data.markdownRemark,r=t.frontmatter.title,c=t.frontmatter.description||t.excerpt,i=t.html;return a.createElement(n.Z,null,a.createElement(l.Z,{title:r,description:c,html:i}))}}}]);
-//# sourceMappingURL=component---src-templates-static-site-js-77fd950bbc51256da717.js.map
