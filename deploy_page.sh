3d1fa0beac56857497624f2d53dcffe8d0c5800e
Lukas Woodtli <woodtli.lukas@gmail.com>
2023-06-21 18:01:05 +0200

    Add note for deploying page

--- deploy_page.sh
+++ deploy_page.sh
@@ -13,6 +13,8 @@ npm run build
 
 npm run test
 
+# If `lftp` fails with "Fatal error: Host key verification failed.":
+# log in first manually with `ssh -p 52839 $USER_NAME@server101.hostfactory.ch`
 lftp -p 52839 -u $USER_NAME,$PASSWORD sftp://server101.hostfactory.ch  -e "mirror -R -P 4 $SCRIPTPATH/public /httpdocs ; exit"
 
 npm run e2e:deployed
