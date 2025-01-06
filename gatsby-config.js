fadac251399f1e195e50657d62d276b58bcadcfd
Lukas Woodtli <woodtli.lukas@gmail.com>
2025-01-06 15:24:21 +0100

    Remove unused (commended) offline plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,8 +69,5 @@ module.exports = {
     `gatsby-theme-material-ui`,
     `gatsby-plugin-sitemap`,
     `gatsby-plugin-react-helmet`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
