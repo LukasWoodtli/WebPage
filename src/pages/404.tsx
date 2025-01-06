609b9805c935d905b6d46ad162ed675500ada00c
Lukas Woodtli <woodtli.lukas@gmail.com>
2025-01-06 15:41:12 +0100

    Remove SEO components

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,13 +1,11 @@
 import * as React from "react";
 
 import Layout from "../layout/layout";
-import { GatsbySeo } from "gatsby-plugin-next-seo/src/meta/gatsby-seo";
 
 const NotFoundPage = () => {
 
   return (
     <Layout>
-      <GatsbySeo title="404: Not Found" description="This page doesn't exist"/>
       <h1>404: Not Found</h1>
       <p>You just hit a route that doesn&#39;t exist...</p>
     </Layout>
