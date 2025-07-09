17e358dff92c4517d06f1e15447899c4bc872961
Lukas Woodtli <woodtli.lukas@gmail.com>
2025-07-09 21:19:44 +0200

    Differentiate between internal and external links

--- src/components/link.tsx
+++ src/components/link.tsx
@@ -3,7 +3,14 @@ import MuiLink from "@mui/material/Link"
 import { Link as GatsbyLink } from "gatsby"
 
 const Link = React.forwardRef(function Link(props, ref) {
-  return <MuiLink component={GatsbyLink} ref={ref} {...props} />
+  // inspired by: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-link/#reminder-use-link-only-for-internal-links
+  const internal = /^\/(?!\/)/.test(props.to)
+  if (internal) {
+    return <MuiLink component={GatsbyLink} ref={ref} {...props} />
+  }
+  else {
+    return <MuiLink component="a" {...props} />
+  }
 })
 
 export default Link;
